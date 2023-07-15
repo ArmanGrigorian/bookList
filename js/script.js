d12ec1a29c7578d40cd9597e6e104264138e733d
Arman <armangrigorian.work@gmail.com>
2023-07-15 22:04:23 +0400

    got the basic look

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-"use strict"
+"use strict";
+
+const bookSpan = document.getElementById("bookSpan");
+const listSpan = document.getElementById("listSpan");
+
+const changeBookColor = setInterval(() => {
+	bookSpan.style.color = `
+    rgb(${Math.round(Math.random() * 255)},
+        ${Math.round(Math.random() * 255)},
+        ${Math.round(Math.random() * 255)}
+        )`;
+}, 600);
+
+const changelistColor = setInterval(() => {
+	listSpan.style.color = `
+    rgb(${Math.round(Math.random() * 255)},
+        ${Math.round(Math.random() * 255)},
+        ${Math.round(Math.random() * 255)}
+        )`;
+}, 600);
